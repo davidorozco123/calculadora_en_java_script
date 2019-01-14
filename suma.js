f48473bb4477a90d5dde3e14f9c1f102f0564789
davidorozco123 <davidorozcoquiroz2301@gmail.com>
2019-01-14 07:36:20 -0600

    Update suma.js

--- suma.js
+++ suma.js
@@ -1,7 +1,19 @@
-console.log("ingrese el primer numero")
 
-var a=2;
-var b=3;
-var c;
-  c=a+b;
-  console.log (c);
+switch(expression) {
+  case suma:
+  var a=prompt("ingrese el primer numero");
+  var b=prompt("ingrese el segundp numero");
+  var c;
+    c=a+b;
+    console.log (c);
+    break;
+  case resta:
+  var a=prompt("ingrese el primer numero");
+  var b=prompt("ingrese el segundp numero");
+  var c;
+    c=a-b;
+    console.log (c);
+    break;
+  default:
+    // code block
+}
