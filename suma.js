0f862231dfa20cbad9a81149e17d65477d2bad95
davidorozco123 <davidorozcoquiroz2301@gmail.com>
2019-03-21 09:50:03 -0600

    hola

--- suma.js
+++ suma.js
@@ -1,20 +1,4 @@
-
-var leer=prompt('Ingrese la operacion que desea hacer:','');
-switch(leer) {
-  case suma:
-  var a=prompt("ingrese el primer numero");
-  var b=prompt("ingrese el segundp numero");
-  var c;
-    c=a+b;
-    console.log (c);
-    break;
-  case resta:
-  var a=prompt("ingrese el primer numero");
-  var b=prompt("ingrese el segundp numero");
-  var c;
-    c=a-b;
-    console.log (c);
-    break;
-  default:
-    // code block
-}
+var a, b, c;
+a=23;
+b=12;
+c=a+b;
