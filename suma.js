fd89edaaf55917ab5128871858179fe21e90dcc2
davidorozco123 <davidorozcoquiroz2301@gmail.com>
2019-03-25 08:53:59 -0600

    Update suma.js

--- suma.js
+++ suma.js
@@ -1,18 +1,40 @@
-var a, b, c;
-//suma
-a=23;
-b=12;
-c=a+b;
-console.log(c)
-//resta
-c=a-b;
-console.log(c)
-//multiplicacion
-c=a*b;
-console.log(c)
-//exponenciacion
-c=a**b
-console.log(c)
-//divicion
-c=a/b
-console.log(c)
+
+console.log('suma,resta,multiplicacion,divicion,exponente');
+console.log('seleccione una operacion')
+var foo = promt();
+switch (foo) {
+
+  case 'suma': // foo es 0, por lo tanto se cumple la condición y se ejecutara el siguiente bloque
+  c=a+b;
+  console.log(c)
+  // NOTA: el "break" olvidado debería estar aquí
+  break;
+
+  case 1: // No hay sentencia "break" en el 'case 0:', por lo tanto este caso también será ejecutado
+  c=a-b;
+  console.log(c)
+  break; // Al encontrar un "break", no será ejecutado el 'case 2:'
+
+  case 2:
+  c=a-b;
+  console.log(c)
+  break;
+
+  case 3:
+  c=a*b;
+  console.log(c)
+  break;
+
+  case 4:
+  c=a**b
+  console.log(c)
+  break;
+
+  case 4:
+  c=a**b
+  console.log(c)
+  break;
+
+  default:
+    console.log('default');
+}
