10d46f797393ba80f9cfa740ba50f3eb5bc8af26
davidorozco123 <davidorozcoquiroz2301@gmail.com>
2019-03-25 12:07:07 -0600

    Update suma.js

--- suma.js
+++ suma.js
@@ -1,16 +1,19 @@
 
-console.log('suma,resta,multiplicacion,divicion,exponente');
-console.log('seleccione una operacion')
-var foo = promt();
+var a,b,c;
+var foo = '1';
 switch (foo) {
 
   case 'suma': // foo es 0, por lo tanto se cumple la condición y se ejecutara el siguiente bloque
+  a=1250000
+  b=750000
   c=a+b;
   console.log(c)
   // NOTA: el "break" olvidado debería estar aquí
   break;
 
   case 1: // No hay sentencia "break" en el 'case 0:', por lo tanto este caso también será ejecutado
+  a=2000000
+  b=600000
   c=a-b;
   console.log(c)
   break; // Al encontrar un "break", no será ejecutado el 'case 2:'
