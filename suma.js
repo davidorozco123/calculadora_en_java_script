a2dfb3dd3df6c4e77d2814a598c980639795090b
davidorozco123 <davidorozcoquiroz2301@gmail.com>
2019-03-27 07:41:57 -0600

    Update suma.js

--- suma.js
+++ suma.js
@@ -1,41 +1,47 @@
 
 var a,b,c;
-var foo = '1';
+var foo = 'exponente';
 switch (foo) {
 
   case 'suma': // foo es 0, por lo tanto se cumple la condición y se ejecutara el siguiente bloque
-  a=1250000
-  b=750000
+  a=2000000
+  b=600000
   c=a+b;
   console.log(c)
+  console.log('esta es la suma')
   // NOTA: el "break" olvidado debería estar aquí
   break;
 
-  case 1: // No hay sentencia "break" en el 'case 0:', por lo tanto este caso también será ejecutado
+  case 'resta':// No hay sentencia "break" en el 'case 0:', por lo tanto este caso también será ejecutado
   a=2000000
   b=600000
   c=a-b;
   console.log(c)
+  console.log('esta es la resta')
   break; // Al encontrar un "break", no será ejecutado el 'case 2:'
 
-  case 2:
-  c=a-b;
-  console.log(c)
-  break;
-
-  case 3:
+  case 'multiplicacion':
+  a=2000000
+  b=600000
   c=a*b;
   console.log(c)
+  console.log('esta es la divicion')
   break;
 
-  case 4:
-  c=a**b
+  case 'exponente':
+  a=2000000
+  b=10
+  c=a**b;
   console.log(c)
+  console.log('este es el exponente')
   break;
 
-  case 4:
-  c=a**b
+  case 'divicion':
+  a=2000000
+  b=600000
+  c=a/b
   console.log(c)
+  console.log('esta es la divicion')
   break;
 
   default:
